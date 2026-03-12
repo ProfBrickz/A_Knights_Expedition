08cc07e8221ff982297acd86b0363d44cb1698a0
ProfBrickz <79584392+ProfBrickz@users.noreply.github.com>
2026-03-12 19:01:55 -0400

    Made port constant, and changed to 3000

--- src/main/java/edu/ycp/cs320/TBAG/main/Main.java
+++ src/main/java/edu/ycp/cs320/TBAG/main/Main.java
@@ -1,28 +1,31 @@
 package edu.ycp.cs320.TBAG.main;
 
-import org.eclipse.jetty.server.Server;
-
 import java.io.File;
 
+import org.eclipse.jetty.server.Server;
+
 public class Main {
+	final static Integer PORT = 3000;
+
 	public static void main(String[] args) throws Exception {
 		String webappCodeBase = "./war";
 		File warFile = new File(webappCodeBase);
 		Launcher launcher = new Launcher();
 
-		// get a server for port 8081
-		System.out.println("CREATING: web server on port 8081");
-		Server server = launcher.launch(true, 8081, warFile.getAbsolutePath(), "/");
+		// get a server for port
+		System.out.println("CREATING: web server on port " + PORT);
+		Server server = launcher.launch(true, PORT, warFile.getAbsolutePath(), "/");
 
 		// Start things up!
-		System.out.println("STARTING: web server on port 8081");
+		System.out.println("STARTING: web server on port " + PORT);
 		server.start();
 
-		// dump the console output - this will produce a lot of red text - no worries, that is normal
+		// dump the console output - this will produce a lot of red text - no worries,
+		// that is normal
 		server.dumpStdErr();
 
 		// Inform user that server is running
-		System.out.println("RUNNING: web server on port 8081");
+		System.out.println("RUNNING: web server on port " + PORT);
 
 		// The use of server.join() the will make the current thread join and
 		// wait until the server is done executing.
