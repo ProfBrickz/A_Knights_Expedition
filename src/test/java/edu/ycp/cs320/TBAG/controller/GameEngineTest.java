04cf2455f879d3c619a0306d1cdcd1fc6a3ebe32
lwingate-git <lwingate@ycp.edu>
2026-03-12 21:38:34 -0400

    Update GameEngineTest.java

--- src/test/java/edu/ycp/cs320/TBAG/controller/GameEngineTest.java
+++ src/test/java/edu/ycp/cs320/TBAG/controller/GameEngineTest.java
@@ -18,6 +18,45 @@ public class GameEngineTest {
 	public void setUp() {
 		player = new Player();
 		rooms = new HashMap<String, Room>();
+		Room start = new Room("0", "Shore", "You find yourself washed ashore after a shipwreck");
+		Room center = new Room("1", "Center", "You walk inshore and find a crossroads");
+
+		start.setConnection(center, "NORTH");
+
+		center.setConnection(start, "SOUTH");
+
+		rooms.put(start.getID(), start);
+		rooms.put(center.getID(), center);
 		gameEngine = new GameEngine(player, rooms);
+	}
+
+	@Test
+	public void testDirection() {
+		Assertions.assertLinesMatch(gameEngine.);
+	}
+
+	@Test
+	public void testDirectionNull() {
+		Assertions.assertLinesMatch(gameEngine.);
+	}
+
+	@Test
+	public void testPlayerInMatchingRoom() {
+		Assertions.assertLinesMatch();
+	}
+
+	@Test
+	public void testLook() {
+		Assertions.assertLinesMatch(gameEngine.);
+	}
+
+	@Test
+	public void testLookNull() {
+		Assertions.assertLinesMatch(gameEngine.);
+	}
+
+	@Test
+	public void testNullCommand() {
+		Assertions.assertLinesMatch(gameEngine.);
 	}
 }
