0b11364df7da861653cf4797057a4b2348ce99de
ProfBrickz <79584392+ProfBrickz@users.noreply.github.com>
2026-03-28 01:10:37 -0400

    Added examples attribute to commands

--- src/main/java/edu/ycp/cs320/TBAG/controller/Command.java
+++ src/main/java/edu/ycp/cs320/TBAG/controller/Command.java
@@ -3,24 +3,71 @@ package edu.ycp.cs320.TBAG.controller;
 import java.util.List;
 
 public enum Command {
-	MOVE("move", List.of("direction")),
-	LOOK("look", List.of()),
-	INVENTORY("inventory", List.of()),
-	INSPECT_ITEM("inspect", List.of("item name")),
-	SEARCH("search", List.of()),
-	PICKUP("pickup", List.of("item name")),
-	PICKUP_ALL("pickup-all", List.of()),
-	DROP("drop", List.of("item name")),
-	DROP_ALL("drop-all", List.of()),
-	RESTART("restart", List.of()),
-	HELP("help", List.of());
+	MOVE(
+		"move",
+		List.of("direction"),
+		List.of("move north")
+	),
+	LOOK(
+		"look",
+		List.of(),
+		List.of()
+	),
+	INVENTORY(
+		"inventory",
+		List.of(),
+		List.of()
+	),
+	INSPECT_ITEM(
+		"inspect",
+		List.of("item name"),
+		List.of()
+	),
+	SEARCH(
+		"search",
+		List.of(),
+		List.of()
+	),
+	PICKUP(
+		"pickup",
+		List.of("item name"),
+		List.of()
+	),
+	PICKUP_ALL(
+		"pickup-all",
+		List.of(),
+		List.of()
+	),
+	DROP(
+
+		"drop",
+		List.of("item name"),
+		List.of()
+	),
+	DROP_ALL(
+		"drop-all",
+		List.of(),
+		List.of()
+	),
+	RESTART(
+		"restart",
+		List.of(),
+		List.of()
+	),
+	HELP(
+		"help",
+		List.of(),
+		List.of()
+	);
 
 	private final String command;
 	private final List<String> arguments;
+	private final List<String> examples;
 
-	private Command(String command, List<String> arguments) {
+	private Command(String command, List<String> arguments, List<String> examples) {
 		this.command = command;
 		this.arguments = arguments;
+		this.examples = examples;
 	}
 
 	public String getCommand() {
@@ -31,14 +78,29 @@ public enum Command {
 		return arguments;
 	}
 
+	public List<String> getExamples() {
+		return examples;
+	}
+
 	public String getFormat() {
-		String format = "\"" + this.command;
+		StringBuilder format = new StringBuilder("\"" + this.command);
 
 		for (String argument : this.arguments) {
-			format += " <" + argument + ">";
+			format.append(" <").append(argument).append(">");
 		}
-		format += "\"";
+		format.append("\"");
 
-		return format;
+		if (!this.examples.isEmpty()) {
+			format.append("\n  Examples: ");
+
+			for (String example : examples) {
+				format
+					.append("\n  - \"")
+					.append(example)
+					.append("\"");
+			}
+		}
+
+		return format.toString();
 	}
 }
