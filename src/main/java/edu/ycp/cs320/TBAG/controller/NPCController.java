27d2b5cc633c6aa7777c70b8449316e875c6dd5b
jshue3 <shuejm03@gmail.com>
2026-03-29 18:12:59 -0400

    Added buy method

--- src/main/java/edu/ycp/cs320/TBAG/controller/NPCController.java
+++ src/main/java/edu/ycp/cs320/TBAG/controller/NPCController.java
@@ -2,6 +2,7 @@ package edu.ycp.cs320.TBAG.controller;
 
 import edu.ycp.cs320.TBAG.model.Item;
 import edu.ycp.cs320.TBAG.model.NPC;
+import edu.ycp.cs320.TBAG.model.NPCItem;
 import edu.ycp.cs320.TBAG.model.Player;
 
 public class NPCController {
@@ -10,8 +11,20 @@ public class NPCController {
 	}
 
 	public Item buy(NPC npc, Player player, Integer npcItemId) {
-		throw new UnsupportedOperationException("TODO - implement");
+		 NPCItem npcItem = npc.getItems().get(String.valueOf(npcItemId));
+    	if (npcItem == null) {
+        	return null;
+		}
+		int price = (int)(npcItem.getItem().getValue() * 1.25);
+    	if (player.getCoins() < price) {
+        	return null;
+    	}
+		player.setCoins(player.getCoins() - price);
+    	player.getInventory().addItem(npcItem.getItem());
+
+    	return npcItem.getItem();
 	}
+	
 
 	public Integer sell(NPC npc, Player player, Item item) {
 		Item playerItem = player.getInventory().getItems().get(item.getId());
