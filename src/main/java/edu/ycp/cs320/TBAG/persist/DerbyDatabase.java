a487e7e978cb64d22f5a16f0e21ebbf5617fde89
ProfBrickz <79584392+ProfBrickz@users.noreply.github.com>
2026-04-13 11:40:59 -0400

    Added dialog database test

--- src/main/java/edu/ycp/cs320/TBAG/persist/DerbyDatabase.java
+++ src/main/java/edu/ycp/cs320/TBAG/persist/DerbyDatabase.java
@@ -72,7 +72,7 @@ public class DerbyDatabase implements Database {
 
 	/// From lab 7
 	private Connection connect() throws SQLException {
-		Connection conn = DriverManager.getConnection("jdbc:derby:database.db;create=true");
+		Connection conn = DriverManager.getConnection("jdbc:derby:" + databasePath + ";create=true");
 
 		// Set autocommit too false to allow execution of
 		// multiple queries/statements as part of the same transaction.
@@ -92,6 +92,32 @@ public class DerbyDatabase implements Database {
 		db.loadInitialData();
 
 		System.out.println("Library DB successfully initialized!");
+	}
+
+	public void shutdown() {
+		try {
+			DriverManager.getConnection("jdbc:derby:" + databasePath + ";shutdown=true");
+		} catch (SQLException e) {
+			String state = e.getSQLState();
+
+			// These BOTH indicate successful shutdown
+			if ("XJ015".equals(state) || "08006".equals(state)) {
+				return; // success
+			}
+
+			e.printStackTrace(); // helpful while debugging
+			throw new RuntimeException("Derby shutdown failed: " + state, e);
+		}
+	}
+
+	private final String databasePath;
+
+	public DerbyDatabase(String databasePath) {
+		this.databasePath = databasePath;
+	}
+
+	public DerbyDatabase() {
+		this("database.db");
 	}
 
 
---
+++ src/test/java/edu/ycp/cs320/TBAG/controller/DatabaseTest.java
@@ -0,0 +1,89 @@
+package edu.ycp.cs320.TBAG.controller;
+
+import edu.ycp.cs320.TBAG.persist.Database;
+import edu.ycp.cs320.TBAG.persist.DatabaseProvider;
+import edu.ycp.cs320.TBAG.persist.DerbyDatabase;
+import edu.ycp.cs320.TBAG.persist.FakeDatabase;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+public class DatabaseTest {
+	private final String databasePath = "test-database.db";
+	private Database database;
+
+
+	@Nested
+	class DerbyTests {
+		@BeforeEach
+		public void setUp() {
+			DatabaseProvider.setInstance(new DerbyDatabase(databasePath));
+			database = DatabaseProvider.getInstance();
+			((DerbyDatabase) database).createTables();
+		}
+
+		@AfterEach
+		public void tearDown() throws IOException {
+			((DerbyDatabase) database).shutdown();
+			deleteDirectory(new File(databasePath));
+
+			Assertions.assertFalse(Files.exists(Path.of(databasePath)));
+		}
+
+		@Test
+		public void dialog() {
+			dialogTest();
+		}
+	}
+
+
+	@Nested
+	class FakeTests {
+		@BeforeEach
+		public void setUp() {
+			DatabaseProvider.setInstance(new FakeDatabase());
+			database = DatabaseProvider.getInstance();
+		}
+
+		@Test
+		public void dialog() {
+			dialogTest();
+		}
+	}
+
+	private void dialogTest() {
+		HashMap<Integer, String> dialog = database.getDialog();
+		Assertions.assertTrue(dialog.isEmpty());
+
+		database.addDialog("a");
+		dialog = database.getDialog();
+		Assertions.assertEquals(1, dialog.size());
+		Assertions.assertEquals("a", dialog.get(0));
+
+		database.addDialog("b");
+		dialog = database.getDialog();
+		Assertions.assertEquals(2, dialog.size());
+		Assertions.assertEquals("a", dialog.get(0));
+		Assertions.assertEquals("b", dialog.get(1));
+	}
+
+
+	/// From https://stackoverflow.com/a/29175213
+	private static void deleteDirectory(File file) {
+		File[] contents = file.listFiles();
+
+		if (contents != null) {
+			for (File f : contents) {
+				if (!Files.isSymbolicLink(f.toPath())) {
+					deleteDirectory(f);
+				}
+			}
+		}
+
+		file.delete();
+	}
+}
